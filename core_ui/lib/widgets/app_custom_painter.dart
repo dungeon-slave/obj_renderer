fe9ed980cd7beab292a96599ec03b3b967c62142
Ilona Bondarovich <142791595+ilona-bondarovich@users.noreply.github.com>
2023-10-12 12:00:32 +0300

    add dda algorithm to object rendering

--- core_ui/lib/widgets/app_custom_painter.dart
+++ core_ui/lib/widgets/app_custom_painter.dart
@@ -1,9 +1,14 @@
+import 'dart:math';
+import 'dart:ui';
+
 import 'package:data/data.dart' hide Colors;
 import 'package:flutter/material.dart';
 
 class AppCustomPainter extends CustomPainter {
   final Map<int, List<Vector4>> _entities;
-  final Paint _paint = Paint()..color = Colors.black;
+  final Paint _paint = Paint()
+    ..color = Colors.black
+    ..strokeWidth = 1;
 
   AppCustomPainter({
     required Map<int, List<Vector4>> entities,
@@ -11,15 +16,36 @@ class AppCustomPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, _) {
+    final List<Offset> points = [];
     for (List<Vector4> entity in _entities.values) {
       for (int i = 0; i < entity.length - 1; i++) {
-        canvas.drawLine(
-          Offset(entity[i].x, entity[i].y),
-          Offset(entity[i + 1].x, entity[i + 1].y),
-          _paint,
-        );
+        double xStart = entity[i].x.roundToDouble();
+        double yStart = entity[i].y.roundToDouble();
+
+        double xEnd = entity[i + 1].x.roundToDouble();
+        double yEnd = entity[i + 1].y.roundToDouble();
+
+        double L = max((xEnd - xStart).abs(), (yEnd - yStart).abs());
+
+        double dX = (entity[i + 1].x - entity[i].x) / L;
+        double dY = (entity[i + 1].y - entity[i].y) / L;
+
+        double x = entity[i].x;
+        double y = entity[i].y;
+
+        for (int i = 0; i <= L; i++) {
+          x += dX;
+          y += dY;
+
+          double xi = x.floorToDouble();
+          double yi = y.floorToDouble();
+
+          points.add(Offset(xi, yi));
+        }
       }
     }
+
+    canvas.drawPoints(PointMode.points, points, _paint);
   }
 
   @override
