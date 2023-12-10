318e4f6aece27aeed74b6e8d620c36db18f85e8b
Vasilevsky Matvey <matvey.vasilevsky@innowise-group.com>
2023-12-10 14:23:23 +0100

    deprecated painter

--- data/lib/parser/texture_parser.dart
+++ data/lib/parser/texture_parser.dart
@@ -1,3 +1,30 @@
-class TextureParser {
+import 'dart:io';
+import 'dart:typed_data';
 
+import 'package:bitmap/bitmap.dart';
+import 'package:flutter/cupertino.dart';
+
+abstract class TextureParser {
+  static Future<Map<String, Uint8List>> parseTexture({
+    required String normalPath,
+    required String mirrorPath,
+    required String diffusePath,
+  }) {
+    return Future<Map<String, Uint8List>>(
+      () async {
+        return {
+          'diffuse': await _parseTextureFile(diffusePath),
+          'mirror': await _parseTextureFile(mirrorPath),
+          'normal': await _parseTextureFile(diffusePath),
+        };
+      },
+    );
+  }
+
+  static Future<Uint8List> _parseTextureFile(String path) async {
+    final ImageProvider textureProvider = Image.file(File(path)).image;
+    final Bitmap bitmap = await Bitmap.fromProvider(textureProvider);
+
+    return bitmap.content;
+  }
 }
